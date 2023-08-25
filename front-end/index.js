0128bab4bd81d4ffa098e643c4581b05489f3d7d
Jean-Sébastien <jsbevilacqua@linagora.com>
2023-08-25 11:26:31 +0200

    feat(front-end): load css ressources from index.js

--- front-end/index.js
+++ front-end/index.js
@@ -1,3 +1,7 @@
+import 'milligram/dist/milligram.min.css'
+import 'tui-pagination/dist/tui-pagination.min.css'
+import './css/style.css'
+
 import SessionController from './src/sessionController.js'
 
 

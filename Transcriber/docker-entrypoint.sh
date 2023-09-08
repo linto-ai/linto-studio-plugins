94b844c248b19675aaaa87df7502a4e76eebbc98
Jean-Sébastien <jsbevilacqua@linagora.com>
2023-09-08 16:08:00 +0200

    feat(dockerization): Add all DockerFile and entrypoints

---
+++ Transcriber/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+#echo "Waiting mqtt server..."
+#echo " $BROKER_HOST:$BROKER_PORT "
+#/wait-for-it.sh $BROKER_HOST:$BROKER_PORT --timeout=20 -s -- echo " $BROKER_HOST:$BROKER_PORT is up"
+
+echo "RUNNING : $1"
+cd /usr/src/app/transcriber
+
+eval "$1"
---
+++ front-end/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# build the front-end
+cd /usr/src/app/front-end && npm run build
+cp -r /usr/src/app/front-end/dist/* /var/www/html/
+
+# update nginx conf file
+envsubst '${FRONT_END_PORT}' < /etc/nginx/sites-enabled/frontend.nginx.conf | sponge /etc/nginx/sites-enabled/frontend.nginx.conf
+
+echo "RUNNING : $1"
+eval "$1"

5ea68fe820ea98ded23dfd6122dade68ed1caf9b
Jean-Sébastien <jsbevilacqua@linagora.com>
2023-10-09 14:26:51 +0200

    fix(Session-API): fix swagger not working behind traefik

--- Session-API/components/WebServer/routes/api-docs/index.js
+++ Session-API/components/WebServer/routes/api-docs/index.js
@@ -3,9 +3,16 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger/swagger.json');
 const swaggerJsdoc = require("swagger-jsdoc")
 
+const sessionBasePath = process.env.SESSION_API_BASE_PATH || ''
+
+function forwardedPrefixMiddleware(req, res, next) {
+    req.originalUrl = (req.headers['x-forwarded-prefix'] || '') + req.originalUrl
+    next()
+}
+
 module.exports = (webserver) => {
 
-    swaggerDocument.definition.servers = [{"url": process.env.SESSION_API_HOST + ":" + process.env.SESSION_API_WEBSERVER_HTTP_PORT + "/v1"}]
+    swaggerDocument.definition.servers = [{"url": process.env.SESSION_API_HOST + sessionBasePath + "/v1"}]
     swaggerDocument.definition.components = {
         ...swaggerDocument.definition.components,
         ...require('./swagger/components/index.js')
@@ -20,6 +27,6 @@ module.exports = (webserver) => {
     swaggerDocument.apis = ["./swagger/"]
 
     //serve swagger
-    webserver.express.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(swaggerDocument)));
+    webserver.express.use('/api-docs/', forwardedPrefixMiddleware, swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(swaggerDocument)));
 }
 

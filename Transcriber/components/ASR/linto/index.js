c344a430d750fae3d45c68ec35082b9217508ba6
Jean-Sébastien <jsbevilacqua@linagora.com>
2024-06-11 15:37:10 +0200

    Revert "Transcriber: fix linto ASR, close was mispelled"
    This reverts commit d55c3831936edc21bfb79ad34a334cd4fc8eb276.

--- Transcriber/components/ASR/linto/index.js
+++ Transcriber/components/ASR/linto/index.js
@@ -73,8 +73,6 @@ class LintoTranscriber extends EventEmitter {
     transcribe(buffer) {
         if (this.ws) {
             this.ws.send(buffer);
-        } else {
-            debug("Trying to send a buffer without websocket");
         }
     }
 

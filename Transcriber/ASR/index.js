2367055f8ce667fa153263ef8e4acbaaf459822b
damienlaine <damien.laine@gmail.com>
2024-07-09 01:22:19 +0200

    Added keepAudio transcoding to MP3. Added ffmpeg using fluent-ffmpeg - @todo add ffmpeg in docker image

--- Transcriber/ASR/index.js
+++ Transcriber/ASR/index.js
@@ -3,6 +3,7 @@ const eventEmitter = require('eventemitter3');
 const path = require('path');
 const fs = require('fs');
 const { CircularBuffer } = require("live-srt-lib");
+const ffmpeg = require('fluent-ffmpeg');
 const ASR_ERROR = require('./error.js')
 
 
@@ -13,6 +14,26 @@ function loadAsr(provider) {
   }
   const AsrClass = require(asrPath);
   return AsrClass;
+}
+
+//@TODO: add ffmpeg in docker builds
+function transcodeToMp3(inputPath, outputPath) {
+  return new Promise((resolve, reject) => {
+    ffmpeg(inputPath)
+      .inputFormat('s16le')      // Specify the input format as signed 16-bit little-endian PCM
+      .inputOptions(['-ar 16000', '-ac 1'])
+      .audioCodec('libmp3lame')
+      .audioBitrate('64k')
+      .on('end', () => {
+        console.log(`Transcoding to MP3 completed: ${outputPath}`);
+        resolve();
+      })
+      .on('error', (err) => {
+        console.error(`Error during transcoding: ${err.message}`);
+        reject(err);
+      })
+      .save(outputPath);
+  });
 }
 
 class ASR extends eventEmitter {
@@ -38,9 +59,10 @@ class ASR extends eventEmitter {
     // identifies the transcriber profile for the channel channelIndex in the session channels array
     try {
       const channel = this.session.channels.find(c => c.index === this.channelIndex);
-      
-      if (channel.keepAudio){
-        this.audioFile = fs.createWriteStream(`${this.session.id}-${this.channelIndex}.pcm`);
+
+      if (channel.keepAudio) {
+        const audioFilePath = path.join(process.env.AUDIO_STORAGE_PATH, `${this.session.id}-${this.channelIndex}.pcm`);
+        this.audioFile = fs.createWriteStream(audioFilePath);
         this.keepAudio = true;
       }
       this.audioBuffer = new CircularBuffer();
@@ -68,12 +90,12 @@ class ASR extends eventEmitter {
     this.provider.on('error', error => {
       const msg = ASR_ERROR[error]
       const final = {
-          "astart": this.startedAt,
-          "text": msg,
-          "start": Math.floor(new Date().getTime() / 1000) - this.startTimestamp,
-          "end": Math.floor(new Date().getTime() / 1000) - this.startTimestamp,
-          "lang": 'EN-en',
-          "locutor": process.env.TRANSCRIBER_BOT_NAME
+        "astart": this.startedAt,
+        "text": msg,
+        "start": Math.floor(new Date().getTime() / 1000) - this.startTimestamp,
+        "end": Math.floor(new Date().getTime() / 1000) - this.startTimestamp,
+        "lang": 'EN-en',
+        "locutor": process.env.TRANSCRIBER_BOT_NAME
       }
       this.emit('final', final)
       console.error(msg)
@@ -92,10 +114,15 @@ class ASR extends eventEmitter {
     });
   }
 
-  async dispose(){
+  async dispose() {
     try {
       if (this.audioFile) {
         this.audioFile.close();
+        const pcmFilePath = path.join(process.env.AUDIO_STORAGE_PATH, `${this.session.id}-${this.channelIndex}.pcm`);
+        const mp3FilePath = path.join(process.env.AUDIO_STORAGE_PATH, `${this.session.id}-${this.channelIndex}.mp3`);
+        await transcodeToMp3(pcmFilePath, mp3FilePath);
+        debug(`Audio file saved as ${mp3FilePath}`);
+        fs.unlinkSync(pcmFilePath);
       }
       if (this.provider) {
         this.provider.removeAllListeners();
@@ -111,12 +138,12 @@ class ASR extends eventEmitter {
     this.removeAllListeners();
     return true
   }
-  
+
   transcribe(buffer) {
     this.audioBuffer.add(buffer);
     if (!(this.state === ASR.states.READY || this.state === ASR.states.TRANSCRIBING)) return;
     const audioBuffer = this.audioBuffer.getAudioBuffer();
-    if (audioBuffer.length >= Math.floor(process.env.MIN_AUDIO_BUFFER/1000 * process.env.SAMPLE_RATE * process.env.BYTES_PER_SAMPLE)) {
+    if (audioBuffer.length >= Math.floor(process.env.MIN_AUDIO_BUFFER / 1000 * process.env.SAMPLE_RATE * process.env.BYTES_PER_SAMPLE)) {
       if (this.keepAudio) {
         this.audioFile.write(audioBuffer);
       }

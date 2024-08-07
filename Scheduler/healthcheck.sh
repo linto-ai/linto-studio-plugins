7bf0f5df346af11971bcab9c21f8e1367ce89a9f
Jean-Sébastien <jsbevilacqua@linagora.com>
2024-08-07 18:20:13 +0200

    Scheduler: fix docker healthcheck

--- Scheduler/healthcheck.sh
+++ Scheduler/healthcheck.sh
@@ -1,2 +1,1 @@
-mosquitto_sub -h $BROKER_HOST -p $BROKER_PORT -t scheduler/status -C 1 | grep '"online":true'
-curl --fail http://localhost:$SCHEDULER_WEBSERVER_HTTP_PORT/healthcheck || exit 1
+mosquitto_sub -h $BROKER_HOST -p $BROKER_PORT -t scheduler/status -C 1 | grep '"online":true' || exit 1

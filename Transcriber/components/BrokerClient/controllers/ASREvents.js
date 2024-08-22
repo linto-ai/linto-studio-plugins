8d35605c240abe3125b4813251fbbd542deb3330
Jean-Sébastien <jsbevilacqua@linagora.com>
2024-08-22 14:26:29 +0200

    Transcriber: set the SessionId and ChannelIndex in the MQTT topic

--- Transcriber/components/BrokerClient/controllers/ASREvents.js
+++ Transcriber/components/BrokerClient/controllers/ASREvents.js
@@ -3,10 +3,10 @@ const debug = require('debug')(`transcriber:BrokerClient:ASREvents`);
 //here, "this" is bound to the BrokerClient component
 module.exports = async function () {
     this.app.components['StreamingServer'].on('partial', (transcription, sessionId, channelIndex) => {
-        this.client.publish('partial', {transcription, sessionId, channelIndex}); // Require online TRUE, we don't want to publish if we are offline
+        this.client.publish(`transcriber/out/${sessionId}/${channelIndex}/partial`, transcription); // Require online TRUE, we don't want to publish if we are offline
     });
 
     this.app.components['StreamingServer'].on('final', (transcription, sessionId, channelIndex) => {
-        this.client.publish('final', {transcription, sessionId, channelIndex}); //require online FALSE, if we are offline, we want to publish anyway, packet will be queued by MQTT client and sent when we are online again, Packet ids are a 16bit number (max 65535) and must be unique for all inflight messages.
+        this.client.publish(`transcriber/out/${sessionId}/${channelIndex}/final`, transcription); //require online FALSE, if we are offline, we want to publish anyway, packet will be queued by MQTT client and sent when we are online again, Packet ids are a 16bit number (max 65535) and must be unique for all inflight messages.
     });
 }

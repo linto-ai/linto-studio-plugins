4bc19bdb9c35353ed65e7f1d89a0232488c32f62
Jean-Sébastien <jsbevilacqua@linagora.com>
2025-01-23 15:18:17 +0100

    Session-API: properly emits log in middleware

--- Session-API/components/WebServer/middlewares/index.js
+++ Session-API/components/WebServer/middlewares/index.js
@@ -2,8 +2,31 @@ const { logger: appLogger } = require('live-srt-lib')
 
 
 function logger(req, res, next) {
-    appLogger.debug(`[${Date.now()}] new user entry on ${req.url}`)
-    next()
+    appLogger.debug(`[${Date.now()}] new user entry on ${req.url}`);
+
+    // log 400 error
+    const originalJson = res.json;
+    res.json = function (body) {
+      if (body.error) {
+          appLogger.warn(body.error);
+      }
+      return originalJson.call(this, body);
+    };
+
+    // log 404 error
+    const originalSend = res.send;
+    res.send = function (body) {
+      res.locals.responseBody = body;
+      return originalSend.call(this, body); // Continue le comportement normal
+    };
+
+    res.on('finish', () => {
+      if (res.statusCode >= 400 && res.statusCode < 500 && res.locals.responseBody) {
+        appLogger.warn(res.locals.responseBody);
+      }
+    });
+
+    next();
 }
 
 module.exports = {

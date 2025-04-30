c26ab4b54862403e1cc02d42ecf8279574310c7c
Jean-Sébastien <jsbevilacqua@linagora.com>
2025-04-30 11:28:16 +0200

    Transcriber: move default audio storage path to /audio-storage

--- Transcriber/docker-entrypoint.sh
+++ Transcriber/docker-entrypoint.sh
@@ -56,7 +56,8 @@ function setup_user() {
     chmod -R u+rwx "$USER_HOME"
 
     # Grant full permisisions on AUDIO_STORAGE_PATH
-    AUDIO_STORAGE_PATH="${AUDIO_STORAGE_PATH:-/tmp}"
+    AUDIO_STORAGE_PATH="${AUDIO_STORAGE_PATH:-/audio-storage}"
+    mkdir -p $AUDIO_STORAGE_PATH
     chown "$USER_NAME:$GROUP_NAME" "$AUDIO_STORAGE_PATH"
     chmod -R u+rwx "$AUDIO_STORAGE_PATH"
 }

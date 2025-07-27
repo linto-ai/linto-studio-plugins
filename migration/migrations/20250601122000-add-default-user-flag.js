691da8b59f97cccfbbb79a864de8c671944915ca
Jean-Sébastien <jsbevilacqua@linagora.com>
2025-07-27 11:46:06 +0200

    Handle default user update for Teams webhook (#6)
    * feat(msteams): track default user
    
    * Update index.js

---
+++ migration/migrations/20250601122000-add-default-user-flag.js
@@ -0,0 +1,14 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('msTeamsUsers', 'defaultUser', {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('msTeamsUsers', 'defaultUser');
+  }
+};

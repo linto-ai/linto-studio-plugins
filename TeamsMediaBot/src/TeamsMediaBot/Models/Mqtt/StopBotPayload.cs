0456d0723b813e1454ad01f976b8755db8203c10
js <jsbevilacqua@linagora.com>
2025-12-15 09:02:42 +0000

    [TeamsMediaBot] Add flexible JSON converters and TranscriberHost override
    - Add StringOrNumberConverter for session/channel IDs that can be string or number
    - Add FlexibleLongConverter for timestamps that can be integer or decimal
    - Add TranscriberHost setting to override WebSocket host in URLs

--- TeamsMediaBot/src/TeamsMediaBot/Models/Mqtt/StopBotPayload.cs
+++ TeamsMediaBot/src/TeamsMediaBot/Models/Mqtt/StopBotPayload.cs
@@ -11,12 +11,14 @@ namespace TeamsMediaBot.Models.Mqtt
         /// The session ID to stop.
         /// </summary>
         [JsonPropertyName("sessionId")]
+        [JsonConverter(typeof(StringOrNumberConverter))]
         public string SessionId { get; set; } = null!;
 
         /// <summary>
         /// The channel ID to stop.
         /// </summary>
         [JsonPropertyName("channelId")]
+        [JsonConverter(typeof(StringOrNumberConverter))]
         public string ChannelId { get; set; } = null!;
     }
 }
